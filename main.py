09e4a96c439f206ba359d5ecc88089220be58fa4
msaka-git <mufitsaka@gmail.com>
2022-05-01 16:25:42 +0200

    design created

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #/bin/env python
+
+from gui_lib import gui_code
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = gui_code.QtWidgets.QApplication(sys.argv)
+    Form = gui_code.QtWidgets.QWidget()
+    ui = gui_code.Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+
+    sys.exit(app.exec_())
+
